994509b3c37669902d9e831302fe40e295abc170
SegunNano <segunfadipe97@gmail.com>
2025-03-14 09:15:03 +0100

    deployment 19

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,7 +1,6 @@
 import { MongoClient, MongoClientOptions } from "mongodb";
 
 const MONGODB_URI = process.env.MONGODB_URI as string;
-const options: MongoClientOptions = {};
 
 if (!MONGODB_URI) {
     throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
